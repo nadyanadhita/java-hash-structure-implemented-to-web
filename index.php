cd478d8949509f478135a63e9d0f1bc12f8f0544
Nadya Nadhita <63330161+nadyanadhita@users.noreply.github.com>
2020-06-04 17:53:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="assets\bootstrap\css\bootstrap.min.css">
+	<link rel="stylesheet" href="assets\css\fontawesome-free\css\all.min.css">
+	<link rel="stylesheet" href="assets\css\style.css">
+	<title>Data Editor</title>
+</head>
+
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		<a class="navbar-brand font-weight-bold" href="#"><i class="fas fa-edit"></i> DATA EDITOR</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="cari.php">PENCARIAN EDITOR</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="tampil.php">TAMPILKAN EDITOR</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="about.php">ABOUT ME</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<!-- End Navbar -->
+
+	<!-- Jumbotron -->
+	<div class="jumbotron mb-0">
+		<h1 class="display-4">Welcome to my Website!</h1>
+		<p class="lead">Website ini menampilkan informasi editor pada artikel situs detik.com beserta artikel yang telah diedit<br>Data yang diperoleh berasal dari struktur data hash yang diintegrasi dengan Web</p>
+		<hr class="my-4">
+		<p>Website ini dibuat untuk memenuhi tugas Projek Lab Matakuliah Struktur Data dan Algoritma Program Studi Informatika Unsyiah</p>
+		<p>Ayo kunjungi github dibawah ini!</p>
+		<a class="btn btn-primary btn-lg" href="https://github.com/nadyanadhita/java-hash-structure-implemented-to-web" role="button"><i class="fab fa-github"></i> Kunjungi Github</a>
+	</div>
+	<!-- End Jumbotron -->
+
+	<script src="assets\bootstrap\js\jquery.min.js"></script>
+	<script src="assets\bootstrap\js\bootstrap.min.js"></script>
+
+</body>
+
+</html>
